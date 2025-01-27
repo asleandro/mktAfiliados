e7cd38b42dd487abb8fe3e261828137da563b782
Leandro Alves <tk.leandro@gmail.com>
2025-01-27 09:44:06 -0300

    Alteração no token

--- mktAfiliados.py
+++ mktAfiliados.py
@@ -1,7 +1,7 @@
 import os
 
 TOKEN = os.getenv("TOKEN_AFILIADOS")
-print(f"Debug: TOKEN_AFILIADOS={TOKEN}")  # Apenas para verificar se está pegando o valor
+print(f"Debug: TOKEN_INTELITECH={TOKEN}")  # Apenas para verificar se está pegando o valor
 
 if not TOKEN:
     raise ValueError("TOKEN não encontrado ou inválido.")
