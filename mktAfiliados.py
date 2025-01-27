7d032a2fdc49af9c035302328e5a33c599df72f6
Leandro Alves <tk.leandro@gmail.com>
2025-01-27 17:37:29 -0300

    testando sem async

--- mktAfiliados.py
+++ mktAfiliados.py
@@ -4,6 +4,8 @@ import os, asyncio, logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
 
 TOKEN = os.getenv("TOKEN_INTELITECH")
 CANAL_ID = "@intelitechofertas"
@@ -11,19 +13,20 @@ CANAL_ID = "@intelitechofertas"
 if not TOKEN:
     raise ValueError("TOKEN não encontrado ou inválido.")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Olá! Digite /promo para ver as melhores ofertas!")
+def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    update.message.reply_text("Olá! Digite /promo para ver as melhores ofertas!")
 
-async def main():
+def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
     loop = asyncio.get_running_loop()
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
     except RuntimeError:
         logging.error("Erro ao executar o loop de eventos.")
+    
